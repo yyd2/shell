1dcfcbf2b6f8e8e3c3971f19c202ba23bbb017a0
yyd2 <yyd2423@163.com>
2021-04-02 17:58:03 +0800

    shell

--- scripts/build-amd.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-env RUSTFLAGS="-C target-cpu=native -g" FFI_BUILD_FROM_SOURCE=1 make clean all lotus-bench lotus-shed
-sudo make install
-
-lotus -v
-lotus-miner -v
--- scripts/install-grafana.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-sudo docker pull grafana/grafana:latest-ubuntu
-sudo docker run -d -p 3000:3000 --name grafana --network host grafana/grafana:latest-ubuntu
-
-# import node-exporter dashboard
