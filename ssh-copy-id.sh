1d84614c3edaf2f5dd7321027bbabac4aa24adf7
yyd2 <yyd2423@163.com>
2021-09-10 16:12:10 +0800

    shell    

--- ssh-copy-id.sh
+++ ssh-copy-id.sh
@@ -1,19 +1,12 @@
 #!/usr/bin/env bash
-for i in m50 m51
+function ssh{
+
+host=~/host
+for i in `cat ${host}`
 do
   ssh-keyscan $i >> ~/.ssh/known_hosts
   ssh-copy-id $i
 done
 
-for i in w{60..78};
-do
-  ssh-keyscan $i >> ~/.ssh/known_hosts
-  ssh-copy-id $i
-done
-
-# commit worker
-for i in w{80..84};
-do
-  ssh-keyscan $i >> ~/.ssh/known_hosts
-  ssh-copy-id $i
-done
+}
+host
